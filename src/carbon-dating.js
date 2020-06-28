a685ebeb3690b06c82e7e829c3b31df44012e717
Aleksei Chernyavskii <mylittlebox255@gmail.com>
2020-06-28 23:46:53 +0300

    implement carbon-dating, hanoi-tower, recursive-depth, simple-chain, transform-array

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,13 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (!sampleActivity || typeof sampleActivity !== 'string') {
+    return false;
+  }
+  const activity = parseFloat(sampleActivity);
+  if (isNaN(activity) || activity <= 0 || activity >= MODERN_ACTIVITY) {
+    return false;
+  }
+  return Math.ceil(Math.log(MODERN_ACTIVITY / Number(activity)) / (0.693 / HALF_LIFE_PERIOD));
 };
